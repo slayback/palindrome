1ee440779480319cd5dc9267e4182c0f11ffc250
Zak Slayback <zslayback@gmail.com>
2020-10-20 16:21:21 -0400

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -15,14 +15,22 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    const lettersRegEx = /[a-z]/gi;
+    return (this.content.match(lettersRegEx) || []).join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
+
 }
+
 
 // Defines a TranslatedPhrase object.
 function TranslatedPhrase(content, translation) {
--- test/test.js
+++ test/test.js
@@ -20,7 +20,25 @@ describe("Phrase", function() {
       assert(mixedCase.palindrome());
     });
 
-    it("should return true for a palindrome with punctuation");
-    
+    it("should return true for a palindrome with punctuation", function() {
+      let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+      assert(punctuatedPalindrome.palindrome());
+    });
+
   });
+
+    describe("#letters", function() {
+
+      it("should return only letters", function() {
+        let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
+        assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
+      });
+
+      it("should return the empty string on no match", function() {
+        let noLetters = new Phrase("1234.56");
+        assert.strictEqual(noLetters.letters(), "");
+      });
+
+    });
+
 });
