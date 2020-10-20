59ce637795438f8306b7101eac86692ed05f2796
Zak Slayback <zslayback@gmail.com>
2020-10-20 15:02:38 -0400

    Add testing suite.

---
+++ test/test.js
@@ -0,0 +1,26 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+describe("Phrase", function() {
+
+  describe("#palindrome", function() {
+
+    it("should return false for a non-palindrome", function() {
+      let nonPalindrome = new Phrase("apple");
+      assert(!nonPalindrome.palindrome());
+    });
+
+    it("should return true for a plain palindrome", function(){
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it("should return true for a mixed-case palindrome", function() {
+      let mixedCase = new Phrase("Racecar");
+      assert(mixedCase.palindrome());
+    });
+
+    it("should return true for a palindrome with punctuation");
+    
+  });
+});
